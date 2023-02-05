4c5830979bfeca5687b5ae9165f10df26475701b
Shreyansh Prakash <shreyanshprakash@gentlemachine.local>
2023-02-05 20:14:10 +0530

    [feature/video-card] : Added logic to prevent duplicate video entry to the download queue for #13

--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,5 +1,6 @@
+import { SearchSectionWrapper } from "components/SearchSection/SearchSectionStyles";
 import { useAppDispatch, useAppSelector } from "hooks";
-import { SearchStateModel } from "models";
+import { SearchStateModel } from "./SearchSectionModels";
 import React, {
   ChangeEvent,
   FC,
@@ -10,45 +11,32 @@ import React, {
 import {
   addToDownloadQueue,
   getDownloadQueue,
+  getVideoMap,
 } from "store/slices";
 import styled, { useTheme } from "styled-components";
 import { Button, SearchField, StyledContainer } from "uiLibrary";
 import { isValidVideoUrl } from "utils";
 
 interface Iprops {}
-
-const SearchSectionWrapper = styled.div`
-  margin-bottom: 24px;
-  .search-section {
-    /* width: 60%; */
-    margin: auto;
-    padding: 24px 0px;
-    display: flex;
-
-    .search-container {
-      border: 4px solid ${({ theme }) => theme?.colors?.greenLight};
-      padding: 24px;
-      margin: auto;
-    }
-  }
-`;
-
 export const SearchSection: FC<Iprops> = (props: Iprops): ReactElement => {
-  const downloadQueue = useAppSelector(getDownloadQueue);
+  const videoMap = useAppSelector(getVideoMap);
   const [searchFieldState, setSearchFieldState] = useState<SearchStateModel>(
     new SearchStateModel()
   );
-
   const theme = useTheme();
-
   const dispatch = useAppDispatch();
 
+  /*
+      User event methods
+    */
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     event?.stopPropagation();
     const value = event?.target?.value;
+    console.log(videoMap[value] !== undefined);
     setSearchFieldState({
       searchValue: value,
       isValidValue: isValidVideoUrl(value),
+      isDuplicate: videoMap[value] !== undefined,
     });
   };
 
@@ -56,28 +44,45 @@ export const SearchSection: FC<Iprops> = (props: Iprops): ReactElement => {
     event: MouseEvent<HTMLButtonElement> | undefined
   ): void => {
     event?.preventDefault?.();
+    const { searchValue } = searchFieldState;
+    if (videoMap[searchValue]) {
+      return;
+    }
     dispatch(addToDownloadQueue(searchFieldState?.searchValue));
     setSearchFieldState(new SearchStateModel());
   };
+
+  /*
+   */
 
   return (
     <SearchSectionWrapper>
       <StyledContainer className="search-section">
         <StyledContainer className="search-container" display="flex">
-          <SearchField
-            type="search"
-            name="videoLink"
-            width={theme?.formFieldSizes?.sizeXL}
-            styles={{
-              borderRadius: "4px",
-            }}
-            value={searchFieldState?.searchValue}
-            onChange={handleSearchChange}
-          />
+          <div className="search-input-field-container">
+            <SearchField
+              type="search"
+              name="videoLink"
+              width={theme?.formFieldSizes?.sizeXL}
+              styles={{
+                borderRadius: "4px",
+              }}
+              value={searchFieldState?.searchValue}
+              onChange={handleSearchChange}
+            />
+            <p className="helper-text">
+              {
+                searchFieldState.isDuplicate && 
+                "Video with same link already exist in the queue!"
+              }
+            </p>
+          </div>
           <Button
             label="Download"
             onClick={handleDownloadClick}
-            disabled={!searchFieldState?.isValidValue}
+            disabled={
+              !searchFieldState?.isValidValue || searchFieldState.isDuplicate
+            }
             styles={{
               borderRadius: "8px",
               marginLeft: "16px",
